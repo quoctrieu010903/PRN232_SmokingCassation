60d2a515862c7855e3feed84d9cc85239db354dc
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-05-23 22:33:16 +0700

    feature/Authentication : Refacter code

---
+++ SmokingCessation.Application/Service/Implementations/CurrentUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SmokingCessation.Application.Service.Implementations
+{
+    public record CurrentUser(string UserId, string email, IEnumerable<string> Roles)
+    {
+
+        public bool isInRole(string role) => Roles.Contains(role);
+
+    }
+}
+
--- SmokingCessation.Infrastracture/Extentions/ServiceCollectionExtentions.cs
+++ SmokingCessation.Infrastracture/Extentions/ServiceCollectionExtentions.cs
@@ -1,9 +1,12 @@
 ﻿
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SmokingCessation.Domain.Entities;
 using SmokingCessation.Infrastracture.Persistence;
+
+
 
 
 namespace SmokingCessation.Infrastracture.Extentions
@@ -12,9 +15,16 @@ namespace SmokingCessation.Infrastracture.Extentions
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            
             var connectionString = configuration.GetConnectionString("SmokingCessationDB");
             services.AddDbContext<SmokingCassationDBContext>(options =>
                 options.UseSqlServer(connectionString).EnableSensitiveDataLogging());
+
+            //
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                     .AddEntityFrameworkStores<SmokingCassationDBContext>()
+                     .AddDefaultTokenProviders();
+
         }
     }
 }
