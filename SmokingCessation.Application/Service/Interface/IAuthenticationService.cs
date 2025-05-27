29d94a9e18eb0ac117c6db72e51131996b9a9c99
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-05-27 07:30:05 +0700

    fix comflict

---
+++ SmokingCessation.Application/Service/Interface/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmokingCessation.Application.DTOs.Request;
+using SmokingCessation.Core.Response;
+
+namespace SmokingCessation.Application.Service.Interface
+{
+    public interface IAuthenticationService
+    {
+        Task AssignUserRole(AssignUserRoles request);
+
+    }
+}
