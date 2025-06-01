b909a9bd5344d81d3fcaa51c9af7d8a4174af297
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-06-01 16:26:24 +0700

    Feature/MembershipPackage : Create update delete , paging successfully

---
+++ SmokingCessation.Application/DTOs/Request/PagingRequestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmokingCessation.Core.Constants;
+
+namespace SmokingCessation.Application.DTOs.Request
+{
+    public class PagingRequestModel
+    {
+
+        [Range(1, int.MaxValue, ErrorMessage = "Số trang phải lớn hơn 1")]
+        public int PageNumber { get; set; } = 1;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Sức chứa 1 trang phải lớn hơn 1")]
+        public int PageSize { get; set; } = 20;
+
+    }
+}
---
+++ SmokingCessation.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+using AutoMapper;
+using SmokingCessation.Application.DTOs.Request;
+using SmokingCessation.Application.DTOs.Response;
+using SmokingCessation.Domain.Entities;
+using static SmokingCessation.Application.DTOs.Request.AuthenticationRequest;
+using static SmokingCessation.Application.DTOs.Response.AuthenticationResponse;
+
+namespace SmokingCessation.Application.Mapping
+{
+    public class MappingProfile  : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ApplicationUser, UserResponse>();
+            CreateMap<ApplicationUser, CurrentUserResponse>();
+            CreateMap<UserRegisterRequest, ApplicationUser>();
+
+            #region MemberShipPackage 
+
+            CreateMap<MembershipPackage, MemberShipPackageRequest>();
+            CreateMap<MembershipPackage, MemberShipPackageResponse>()
+                    .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()));
+
+            CreateMap<MemberShipPackageRequest, MembershipPackage>();
+            // In your AutoMapper profile
+            CreateMap<MembershipPackage, MemberShipPackageResponse>();
+                
+
+
+
+
+            #endregion
+        }
+    }
+}
