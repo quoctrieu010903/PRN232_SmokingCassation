f4daead28a375f68e2d36bc1a93f84dd7ba142a1
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-07-05 13:16:50 +0700

    update deploy

--- SmokingCessation.WebAPI/Program.cs
+++ SmokingCessation.WebAPI/Program.cs
@@ -26,7 +26,7 @@ var seeder = scope.ServiceProvider.GetRequiredService<ISmokingSessationSeeder>()
 await seeder.Seed();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
